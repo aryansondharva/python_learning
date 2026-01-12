c3fc45800f739d1302cc54501a6501aed6e6a8b6
ARYAN SONDHARVA <aryansondharva25@gmail.com>
2026-01-12 08:38:49 +0530

    Day 001

---
+++ 01_Basics/arvi.py
@@ -0,0 +1,3 @@
+from hello_arvi import arvi
+
+arvi("Aryan Sondharva")
---
+++ 01_Basics/hello_arvi.py
@@ -0,0 +1,8 @@
+print("ARYAN SONDHARVA")
+print("Hello, Arvi!")
+
+def arvi(n):
+    print(n)
+
+arvi(5)    # python --version 
+
