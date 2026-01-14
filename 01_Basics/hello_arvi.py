e24f62c160db31a3bdda82e2ee3cd872212f9c51
ARYAN SONDHARVA <aryansondharva25@gmail.com>
2026-01-14 18:59:52 +0530

    001

--- 01_Basics/hello_arvi.py
+++ 01_Basics/hello_arvi.py
@@ -6,3 +6,119 @@ def arvi(n):
 
 arvi(5)    # python --version 
 
+arvi_01 = "Arvi 0.1"
+arvi_02 = "Arvi 0.2"
+arvi_03 = "Arvi 0.3"
+
+
+
+
+# Python 3.13.4 (tags/v3.13.4:8a526ec, Jun  3 2025, 17:46:04) [MSC v.1943 64 bit (AMD64)] on win32
+# Type "help", "copyright", "credits" or "license" for more information.
+# Ctrl click to launch VS Code Native REPL
+# Failed calling sys.__interactivehook__
+# Traceback (most recent call last):
+# >>> import os
+# >>> for c in "ARYAN":
+# ... print(c)
+#   File "<stdin>", line 2
+#     print(c)
+#     ^^^^^
+# ...  print(c)
+# A
+# R
+# Y
+# A
+# N
+# N
+# >>> import sys
+# >>> sys.plateform
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# >>> sys.plateform
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+#   File "<stdin>", line 1, in <module>
+#     import platform
+#     ^^^^^^^^^^^^^
+# AttributeError: module 'sys' has no attribute 'plateform'. Did you mean: 'platform'?
+# >>> import sys
+# AttributeError: module 'sys' has no attribute 'plateform'. Did you mean: 'platform'?
+# >>> import sys
+# >>> import sys
+# >>> sys.plateform()
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+#     import platform
+#     ^^^^^^^^^^^^^
+# AttributeError: module 'sys' has no attribute 'plateform'. Did you mean: 'platform'?
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+#     import platform
+# AttributeError: module 'sys' has no attribute 'plateform'. Did you mean: 'platform'?
+# >>> sys.platform
+# 'win32'
+# >>> import sys
+# >>> sys.platform()
+#     import platform
+#     ^^^^^^^^^^^^^^
+# TypeError: 'str' object is not callable
+# >>> import sys
+# >>> >>>
+# >>> import hello_arvi
+# ARYAN SONDHARVA
+# Hello, Arvi!
+# >>> hello_arvi.arvi("NS")
+# >>> hello_arvi.arvi_01
+#   File "<stdin>", line 1, in <module>
+#     import platform
+# AttributeError: module 'hello_arvi' has no attribute 'arvi_01'
+# >>> from importlib import reload
+# >>> hello_arvi.arvi_01
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+#     import platform
+# AttributeError: module 'hello_arvi' has no attribute 'arvi_01'
+# >>> from importlib import reload
+# >>> reload(hello_arvi)
+# ARYAN SONDHARVA
+# Hello, Arvi!
+# 5
+# <module 'hello_arvi' from 'C:\\Users\\ARYAN\\OneDrive\\Desktop\\Python !!\\01_Basics\\hello_arvi.py'>
+# >>> hello_arvi.arvi_01
+# 'Arvi 0.1'
+# >>> hello_arvi.arvi_03
+# 'Arvi 0.3'
+# >>>
+# PS C:\Users\ARYAN\OneDrive\Desktop\Python !!\01_Basics> 
+
+# PS C:\Users\ARYAN\OneDrive\Desktop\Python !!\01_Basics> python
+# Python 3.13.4 (tags/v3.13.4:8a526ec, Jun  3 2025, 17:46:04) [MSC v.1943 64 bit (AMD64)] on win32
+# Type "help", "copyright", "credits" or "license" for more information.
+# Ctrl click to launch VS Code Native REPL
+# PS C:\Users\ARYAN\OneDrive\Desktop\Python !!\01_Basics> python
+# PS C:\Users\ARYAN\OneDrive\Desktop\Python !!\01_Basics> python
+# Python 3.13.4 (tags/v3.13.4:8a526ec, Jun  3 2025, 17:46:04) [MSC v.1943 64 bit (AMD64)] on win32
+# Type "help", "copyright", "credits" or "license" for more information.
+# Ctrl click to launch VS Code Native REPL
+# Failed calling sys.__interactivehook__
+# Traceback (most recent call last):
+#   File "<frozen site>", line 536, in register_readline
+# >>> username = "aryan"
+# >>> username
+# 'aryan'
+# >>> username = "naitik"
+# >>> username
+# 'naitik'
+# >>> x=10
+# >>> y=x
+# >>> x
+# 10
+# >>> y
+# 10
+# >>> x=15
+# >>> y
+# 10
+# >>>
